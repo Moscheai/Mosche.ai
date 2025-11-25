e72b1e5c1b248cb78238d89bcba8c38f03741934
Moscheai <philippschreiner27@gmx.at>
2025-11-25 17:30:31 +0100

    Update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Set your backend URL (Cloudflare Worker / Vercel)
-const BACKEND_URL = "REPLACE_WITH_YOUR_BACKEND_URL"; // e.g. https://moscheai-backend.workers.dev
+const BACKEND_URL = "https://moscheai.philippschreiner27.workers.dev"; // e.g. https://moscheai-backend.workers.dev
 // Optional: set a client-side HuggingFace key (NOT RECOMMENDED)
-const HUGGINGFACE_KEY = ""; 
+const HUGGINGFACE_KEY = "hf_uyoJGzsdrukBHlUIocsjuYr-VTIUzxAZlgV"; 
